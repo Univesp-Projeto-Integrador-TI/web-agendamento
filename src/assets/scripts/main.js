9efb736b160998f0815e2f75b905c6d229668d86
guimanso <gui.2015.mv@gmai.com>
2026-05-22 22:43:57 -0300

    feat: abrir e fechar modal

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,44 @@
+const overlay = document.getElementById("overlay");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+
+openBtn.addEventListener("click", () => {
+    overlay.style.display = "flex";
+    document.body.style.overflow = "hidden";
+});
+
+function closeModal() {
+    overlay.style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+closeBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+        closeModal();
+    }
+});
+
+
+const overlayCard = document.getElementById("overlayCard");
+const openBtnCard = document.getElementById("openModalCard");
+const closeBtnCard = document.getElementById("closeModalCard");
+
+openBtnCard.addEventListener("click", () => {
+    overlayCard.style.display = "flex";
+    document.body.style.overflow = "hidden";
+});
+
+function closeModalCard() {
+    overlayCard.style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+closeBtnCard.addEventListener("click", closeModalCard);
+
+overlayCard.addEventListener("click", (e) => {
+    if (e.target === overlayCard) {
+        closeModalCard();
+    }
+});
